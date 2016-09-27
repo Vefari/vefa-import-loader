f5b2705698c7eb45ad55dd110a4b23b2107a4fe2
Cobey Potter <cobey.potter@wellfireinteractive.com>
2016-09-27 16:20:07 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// TODO: create option to keep templates in file stream
+var utils = require('loader-utils');
+
+module.exports = function(source){
+    this.cacheable && this.cacheable();
+
+    return "";
+}
