264580f482ae90e131f30ed92367de12a1779786
Cobey Potter <cobey.potter@wellfireinteractive.com>
2017-04-13 23:28:26 -0400

    breakout functions to call in various files

--- index.js
+++ index.js
@@ -9,76 +9,98 @@ module.exports = function(source){
     this.cacheable && this.cacheable();
     
     // lets load the shared locals object
-    let locals = this.options.locals; 
+    let loader = utils.getLoaderConfig(this);
+    let locals = loader.locals; 
 
-    if (!locals.load) {
-        
-        // get which config options to load
-        let loader = utils.getLoaderConfig(this, "import");
-        // get the data property of that load configuration
-        let dir = loader.data;
+    const file_to_locals = (ref, dir) => {
         // read the data directory
-        let ref = fs.readdirSync(dir);
-
-        // parse through the data files and add them to the locals object
-        for (let i = 0, len = ref.length; i < len; i++) {
-            let file = ref[i];
-            let file_name = file.split(".")[0];
-
-            if( file_name ){
-                let data = path.resolve(`${dir}/${file}`)
-                this.addDependency( data );
-                let temp = yaml.safeLoad( fs.readFileSync(data, 'utf8') );
-                
-                if (Array.isArray(temp)) {
-                    locals[file_name] = temp;
-                }
-                else {
-                    locals[file_name] = {};
-                    for (let key in temp) {
-                        locals[file_name][key] = temp[key];
-                    }
-                }
-
-                // output to a "json" directory for use elsewhere
-                if (loader.json.dir) {
-                    // create json folder if it doesn't exist
-                    if (!fs.existsSync(loader.json.dir)){
-                        fs.mkdirSync(loader.json.dir);
-                    }
-                    if (!loader.json.output) {
-                        fs.writeFileSync(
-                            `${loader.json.dir}/${file_name}.json`, 
-                            JSON.stringify(locals[file_name])
-                        );
-                    } 
-                    else if (loader.json.output.includes(file_name)) {
-                        fs.writeFileSync(
-                            `${loader.json.dir}/${file_name}.json`, 
-                            JSON.stringify(locals[file_name])
-                        );
-                    }
-                }
-            }
-        }
-
-        // get PAGES loader info, bypass if not there
-        loader = utils.getLoaderConfig(this, "pages");
-        dir = loader.context;
         if (dir) {
-            ref = fs.readdirSync(dir);
-            locals.pages = {};
-            
-            for (let i = 0, len = ref.length; i < len; i++) {
-                let file = ref[i];
-                let file_name = file.split(".")[0];
-                if( file_name ){
-                    let data = path.resolve(`${dir}/${file}`)
-                    this.addDependency( data );
-                    locals.pages[file_name] = matter(fs.readFileSync(data, 'utf8')).data;
+            ref.forEach((file) => {
+                // make sure its a yaml file
+                if( file.includes('yaml') ||  file.includes('yml') ){
+                    let file_name = file.split(".")[0]
+
+                    if (file_name){
+                        let data = path.resolve(`${dir}/${file}`)
+                        this.addDependency( data )
+                        let temp = yaml.safeLoad( fs.readFileSync(data, 'utf8') );
+
+                        if (temp) {
+                            locals[file_name] = temp
+                        }
+                    }
                 }
-            }
+            })
         }
+    }
+
+    const pages_to_locals = (ref, dir) => {
+        // read the pages directory
+        if (dir) {
+            locals.pages = Object.create({})
+            ref.forEach((file) => {
+                // make sure its a yaml file
+                if( file.includes('md') ||  file.includes('markdown') ){
+                    let file_name = file.split(".")[0]
+
+                    if (file_name){
+                        let data = path.resolve(`${dir}/${file}`)
+                        this.addDependency( data )
+                        locals.pages[file_name] = matter(fs.readFileSync(data, 'utf8')).data;
+                    }
+                }
+            })
+
+        }
+    }
+
+    const file_to_json = (ref, dir) => {
+        let output = loader.import.output
+        if (output.dir) {
+            // create json folder if it doesn't exist
+            if (!fs.existsSync(output.dir)) {
+                fs.mkdirSync (output.dir)
+            }
+
+            ref.forEach((file) => {
+                if( file.includes('yaml') ||  file.includes('yml') ){
+                    let file_name = file.split(".")[0]
+
+                    if (!output.files) {
+                        fs.writeFileSync(
+                            `${output.dir}/${file_name}.json`, 
+                            JSON.stringify (locals[file_name])
+                        )
+                    }
+                    else if (output.files.includes(file_name)) {
+                        fs.writeFileSync(
+                            `${output.dir}/${file_name}.json`, 
+                            JSON.stringify(locals[file_name])
+                        );
+                    }
+                }
+            })
+            
+        }
+    }
+    
+    if (!locals.load) {
+        // get which config options to load
+        // get the data property of that load configuration
+        let data_dir = loader.import.data ? loader.import.data : null
+        let data_ref = fs.readdirSync (data_dir)
+        file_to_locals (data_ref, data_dir)
+        
+        // get styles
+        let styles_dir = loader.import.styles ? loader.import.styles : null
+        let styles_ref = fs.readdirSync (styles_dir)
+        file_to_locals (styles_ref, styles_dir)
+        file_to_json (styles_ref, styles_dir)
+
+        // // get PAGES loader info, bypass if not there
+        let pages_dir = (loader.import.pages) ? loader.import.pages : null
+        let pages_ref = fs.readdirSync (pages_dir)
+        pages_to_locals (pages_ref, pages_dir)
 
         // trip load boolean so we don't run the loader for each source file, 
         // but only on initial compilation load
